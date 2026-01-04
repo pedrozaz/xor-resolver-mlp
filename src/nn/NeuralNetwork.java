e5bc1896914feef3698cbf6dfcf70cfdcb7909e4
Pedrozaz <guilhermebarb0sa@proton.me>
2026-01-04 15:05:20 -0300

    implement backpropagation training algorithm

--- src/nn/NeuralNetwork.java
+++ src/nn/NeuralNetwork.java
@@ -56,6 +56,48 @@ public class NeuralNetwork {
         return outputList;
     }
 
+    public void train(double[] input_array, double[] target_array) {
+        Matrix inputs = Matrix.fromArray(input_array);
+
+        Matrix hidden = Matrix.mult(this.weights_ih, inputs);
+        hidden.add(this.bias_h);
+        hidden.map(this::sigmoid);
+
+        Matrix outputs = Matrix.mult(this.weights_ho, hidden);
+        outputs.add(this.bias_o);
+        outputs.map(this::sigmoid);
+
+        Matrix targets = Matrix.fromArray(target_array);
+        Matrix output_errors = Matrix.sub(targets, outputs);
+
+        Matrix gradients = Matrix.map(outputs, this::dsigmoid);
+        gradients.mult(output_errors);
+        gradients.mult(this.learning_rate);
+
+        Matrix hidden_T = Matrix.trans(hidden);
+        Matrix weight_ho_deltas = Matrix.mult(gradients, hidden_T);
+
+        this.weights_ho.add(weight_ho_deltas);
+        this.bias_o.add(gradients);
+
+        Matrix who_T = Matrix.trans(this.weights_ho);
+        Matrix hidden_errors = Matrix.mult(who_T, output_errors);
+
+        Matrix hidden_gradient = Matrix.map(hidden, this::dsigmoid);
+        hidden_gradient.mult(hidden_errors);
+        hidden_gradient.mult(this.learning_rate);
+
+        Matrix inputs_T = Matrix.trans(inputs);
+        Matrix weight_ih_deltas = Matrix.mult(hidden_gradient, inputs_T);
+
+        this.weights_ih.add(weight_ih_deltas);
+        this.bias_h.add(hidden_gradient);
+    }
+
+    private double dsigmoid(double y) {
+        return y * (1 - y);
+    }
+
     private double sigmoid(double x) {
         return 1 / (1 + Math.exp(-x));
     }
